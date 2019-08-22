a6baef04e59e1e1447bc14c2d2dca6e3091feabf
maxiaobu1999 <maqinglong1999@163.com>
2019-08-22 17:22:42 +0800

    【opengl】绘制图片

--- opengl/src/main/java/com/norman/opengl/OpenGLActivity.java
+++ opengl/src/main/java/com/norman/opengl/OpenGLActivity.java
@@ -1,18 +1,23 @@
 package com.norman.opengl;
 
+import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class OpenGLActivity extends AppCompatActivity {
     public static final String TAG = "OpenGLActivity+++:";
+    private Activity mActivity;
     private GLSurfaceView mGlSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_gl);
+        mActivity = this;
         //检测openGL版本
         int glVersion = GLESUtils.getSupportGLVersion(this);
         String msg = "支持 GLES " + glVersion;
@@ -22,7 +27,9 @@ public class OpenGLActivity extends AppCompatActivity {
         // 求一个OpenGL ES 2.0兼容的上下文
         mGlSurfaceView.setEGLContextClientVersion(2);
         GLSurfaceView.Renderer renderer = null;
-        renderer = new TriangleRenderer();
+//        renderer = new TriangleRenderer();
+        File imageFile = new File("/sdcard/norman/media/cat.jpg");
+        renderer = new ImageRenderer(imageFile.getAbsolutePath());
         //设置渲染器
         mGlSurfaceView.setRenderer(renderer);
         mGlSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
